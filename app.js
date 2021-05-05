fd80d6e2b19809cc1eddb2fa80dd6ee83439bf8a
gbhavyy7 <83642196+gbhavyy7@users.noreply.github.com>
2021-05-05 18:39:11 +0530

    done

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const btnTranslate = document.getElementById("btn-translate");
 const textInput = document.getElementById("text-input");
 const output = document.querySelector("#output");
 
+
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL+"?text="+input
+}
+
 function clickHandler(){
-    output.innerHTML=("sdfg "+textInput.value);
+    const inputText=textInput.value;
+    fetch(getTranslationURL(inputText))
+    .then( response => response.json() )
+    .then( json =>{ 
+        const translatedText = json.contents.translated;
+        output.innerText = translatedText;
+        } )
+
+
 };
     
 
